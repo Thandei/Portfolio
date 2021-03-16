4d7ef35017078cf51bc2b3057f556c7675f5ba4c
Thandei <67441515+Thandei@users.noreply.github.com>
2021-03-16 23:53:07 +0300

    singlepost made

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,5 +2,14 @@ import React from "react";
 
 
 export default function Home(){
-    return <h1>Homesss Page</h1>
+    return  (
+        <main>
+            {/* <img /> */}
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8 ">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hi There. I am Ufuk</h1>
+            </section>
+        </main>
+    )
+        
+     
 }
